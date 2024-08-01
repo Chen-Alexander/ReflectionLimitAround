0fb58514f7381984d0f94f20ceb9af38c520ead4
Administrator <1401429109@qq.com>
2024-08-01 17:19:49 +0800

    remove unUse code.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,18 +5,6 @@
 JNIEnv *currentEnv = nullptr;
 JavaVM *globalVM = nullptr;
 
-jobject global_clazz = nullptr;
-jstring global_method_name = nullptr;
-jobjectArray global_params = nullptr;
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_alexander_reflectlimitaround_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
-
 JNIEnv *attachCurrentThread() {
     JNIEnv *env;
     int resCode = globalVM->AttachCurrentThread(&env, nullptr);
@@ -28,50 +16,11 @@ void detachCurrentThread() {
     printf("abc");
 }
 
-//jobject getDeclaredMethod_internal(
-//        jobject clazz,
-//        jstring method_name,
-//        jobjectArray params
-//) {
-//    JNIEnv *env = attachCurrentThread();
-//    jclass clazz_class = env->GetObjectClass(clazz);
-//    jmethodID get_declared_method_id = env->GetMethodID(clazz_class, "getDeclaredMethod",
-//                                                        "(Ljava/lang/String;[Ljava/lang/Class;)Ljava/lang/reflect/Method;");
-//    jobject res = env->CallObjectMethod(clazz, get_declared_method_id, method_name, params);
-//    DetachCurrentThread();
-//    return env->NewGlobalRef(res);
-//}
-
-//extern "C" JNIEXPORT jobject JNICALL
-//Java_com_alexander_reflectlimitaround_MainActivity_getDeclaredMethod(
-//        JNIEnv *env,
-//        jobject interface,
-//        jobject clazz,
-//        jstring method_name,
-//        jobjectArray params
-//        ) {
-////    global_clazz = env->NewGlobalRef(clazz);
-////    global_method_name = (jstring) env->GetStringUTFChars(method_name, nullptr);
-////
-//    auto future = std::async(std::launch::any, &getDeclaredMethod_internal, global_clazz,
-//                             global_method_name, params);
-//    auto result = future.get();
-//
-////    jclass class_clazz = env->GetObjectClass(clazz);
-//    jclass class_clazz = env->FindClass("java/lang/Class");
-//    jmethodID get_declared_method_id = env->GetMethodID(class_clazz, "getDeclaredMethod",
-//                                                        "(Ljava/lang/String;[Ljava/lang/Class;)Ljava/lang/reflect/Method;");
-//    jobject result = env->CallObjectMethod(clazz, get_declared_method_id, method_name, params);
-//
-//    return result;
-//}
-
 extern "C" jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     jint result = -1;
     if (vm->GetEnv((void **) &currentEnv, JNI_VERSION_1_6) != JNI_OK) {
         return result;
     }
-//    globalVM = reinterpret_cast<JavaVM *>(currentEnv->NewGlobalRef(reinterpret_cast<jobject>(vm)));
     globalVM = vm;
     return JNI_VERSION_1_6;
 }
--- app/src/main/java/com/alexander/reflectlimitaround/MainActivity.java
+++ app/src/main/java/com/alexander/reflectlimitaround/MainActivity.java
@@ -25,23 +25,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ActivityMainBinding binding;
-    private static Object vmRuntime;
-    private static Method setHiddenApiExemptions;
-
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            Log.e("MainActivity", "msg.what:" + msg.what);
-        }
-    };
-
-    private Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            Log.e("MainActivity", "I am runnable");
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,36 +33,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
-
         try {
-//            Method forName = Class.class.getDeclaredMethod("forName", String.class);
-//            Method getDeclaredMethod = Class.class.getDeclaredMethod("getDeclaredMethod",
-//                    String.class, Class[].class);
-//            Class<?> vmRunTimeClass = (Class<?>) forName.invoke(null, "dalvik.system.VMRuntime");
-//            Method getRuntime = (Method) getDeclaredMethod.invoke(vmRunTimeClass, "getRuntime", null);
-//            vmRuntime = getRuntime.invoke(null);
-//            setHiddenApiExemptions = (Method) getDeclaredMethod.invoke(vmRunTimeClass, "setHiddenApiExemptions", new Class[]{String[].class});
-//
-//            Looper looper = Looper.getMainLooper();
-//            Method method = Looper.class.getDeclaredMethod("postSyncBarrier", null);
-//            setHiddenApiExemptions.invoke(method);
-
-//            // 单一套娃执行(执行每个方法前都要套娃一次)
-//            handler.post(runnable);
-//            Method forName = Class.class.getDeclaredMethod("forName", String.class);
-//            Method getDeclaredMethod = Class.class.getDeclaredMethod("getDeclaredMethod",
-//                    String.class, Class[].class);
-//            Class<MessageQueue> queueClass = (Class<MessageQueue>) forName.invoke(null, "android.os.MessageQueue");
-//            Method hasMessages = (Method) getDeclaredMethod.invoke(queueClass, "hasMessages", new Class[]{Handler.class, Runnable.class, Object.class});
-//            hasMessages.setAccessible(true);
-//            MessageQueue queue = Looper.getMainLooper().getQueue();
-//            boolean retCode = (boolean) hasMessages.invoke(queue, new Object[]{handler, runnable, null});
-//
-//            handler.sendEmptyMessage(0);
-
             // JNI破坏调用栈，使VM无法识别调用栈，进而无法识别调用方来达成目的()
             setHiddenApiExemptionsNative();
             Choreographer choreographer = Choreographer.getInstance();
@@ -99,6 +53,5 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'reflectlimitaround' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
     public native void setHiddenApiExemptionsNative();
 }
